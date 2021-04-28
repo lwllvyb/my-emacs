b946383115eea0bcc59c2878ed718370d2a1f7b2
wenlongwlli <wenlongwlli@tencent.com>
2021-04-28 16:10:46 +0800

    add

--- lisp/my-packages.el
+++ lisp/my-packages.el
@@ -185,9 +185,9 @@
                 'rust-mode-hook
                 'python-mode-hook
                 'sh-mode-hook
-                'c-mode-common-hook
-                'c-mode-hook
-                'c++-mode-hook
+                ; 'c-mode-common-hook
+                ; 'c-mode-hook
+                ; 'c++-mode-hook
                 ))
    (add-hook hook '(lambda () (nox-ensure))))
 ;;==========================================================================
@@ -793,6 +793,32 @@ perspective."
   (let ((buffer (get-buffer-create "untitled")))
     (set-buffer-major-mode  buffer)
     (display-buffer buffer '(display-buffer-pop-up-window . nil))))
+;;==========================================================================
+(use-package counsel-etags
+  :ensure t
+  :bind (("C-]" . counsel-etags-find-tag-at-point))
+  :init
+  (add-hook 'prog-mode-hook
+        (lambda ()
+          (add-hook 'after-save-hook
+            'counsel-etags-virtual-update-tags 'append 'local)))
+  :config
+  (setq counsel-etags-update-interval 60))
+(with-eval-after-load 'counsel-etags
+  ;; counsel-etags-ignore-directories does NOT support wildcast
+  (push "build" counsel-etags-ignore-directories)
+  (push ".ccls-cache" counsel-etags-ignore-directories)
+  ;; counsel-etags-ignore-filenames supports wildcast
+  (push "TAGS" counsel-etags-ignore-filenames))
+;; Don't ask before rereading the TAGS files if they have changed
+(setq tags-revert-without-query t)
+;; Don't warn when TAGS files are large
+(setq large-file-warning-threshold nil)
+;; Setup auto update now
+(add-hook 'prog-mode-hook
+  (lambda ()
+    (add-hook 'after-save-hook
+              'counsel-etags-virtual-update-tags 'append 'local)))
 ;;==========================================================================
 ;; 文件末尾
 (provide 'my-packages)
